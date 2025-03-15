e947ff7c823545c70bb4cd2634741c061a9831bd
Davron01~ <d.mirabdullayev@mail.ru>
2025-03-16 03:33:18 +0500

    service section tugadi

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,17 +1,21 @@
+"use client";
 import { useState } from "react";
-import { Tabs, TabsContent, TabsTrigger } from "./ui/tabs";
+import { Tabs, TabsContent, TabsTrigger, TabsList } from "./ui/tabs";
 import Button from "./Button";
-import Image from "next/image";
+// import Image from "next/image";
 import {
   PiWallFill,
   PiPaintRollerFill,
   PiWrenchFill,
   PiUserGearFill,
 } from "react-icons/pi";
+import { RiAlignItemBottomFill } from "react-icons/ri";
+import Image from "next/image";
+import PreTitle from "./PreTitle";
 
 const serviceData = [
   {
-    name: "Construction",
+    name: "construction",
     icon: <PiWallFill />,
     title: "Construction Services",
     description:
@@ -49,7 +53,7 @@ const serviceData = [
     ],
   },
   {
-    name: "Restoration",
+    name: "restoration",
     icon: <PiWrenchFill />,
     title: "Restoration Services",
     description:
@@ -68,7 +72,7 @@ const serviceData = [
     ],
   },
   {
-    name: "Consulting",
+    name: "consulting",
     icon: <PiUserGearFill />,
     title: "Consulting Services",
     description:
@@ -89,12 +93,90 @@ const serviceData = [
 ];
 
 const Services = () => {
+  const [activeTab, setActiveTab] = useState("construction");
   return (
-    <div className="" id="services">
-      Services Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-      distinctio et veritatis odit fugit. Corrupti hic nemo qui quidem, odit est
-      accusantium quas dicta eligendi sunt laudantium ex maxime voluptates.
-    </div>
+    <section className="pt-16 xl:pt-32" id="services">
+      <div className="container mx-auto">
+        <div className="mx-auto mb-20 max-w-[540px] text-center">
+          <PreTitle text="Our Services" center />
+          <h2 className="h2 mb-3">Solutions We Provide </h2>
+          <p className="max-w[480px] mx-auto mb-11">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
+            cumque aliquid placeat exercitationem harum. Totam.
+          </p>
+        </div>
+        {/* Tabs */}
+        <Tabs
+          defaultValue="construction"
+          onValueChange={(value) => setActiveTab(value)}
+          className="flex flex-col gap-[30px] xl:flex-row"
+        >
+          <TabsList className="grid h-full w-full grid-cols-1 gap-[30px] rounded-none bg-transparent p-0 md:grid-cols-2 xl:w-[345px] xl:grid-cols-1">
+            {serviceData.map((item) => {
+              return (
+                <TabsTrigger
+                  key={item.name}
+                  value={item.name}
+                  className="relative flex h-[100px] w-full items-center rounded-none p-0 shadow outline-none"
+                >
+                  <div
+                    className={`absolute left-0 flex h-[100px] w-[100px] items-center justify-center ${activeTab === item.name ? "bg-black text-white" : "bg-chart-4 text-primary"}`}
+                  >
+                    <div className="text-4xl">{item.icon}</div>
+                  </div>
+                  <div className="font-primary w-[10px] text-base font-semibold tracking-[.6px] uppercase">
+                    {item.name}
+                  </div>
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          {/* tabcontent */}
+          <div className="h-[490px] flex-1 bg-white p-[30px] shadow">
+            {serviceData.map((item) => (
+              <TabsContent key={item.name} value={item.name} className="m-0">
+                <div className="flex flex-col gap-[30px] md:flex-row">
+                  {/* images */}
+                  <div className="flex gap-5 md:flex-col xl:gap-[30px]">
+                    {item.thumbs.map((thumb, index) => (
+                      <div
+                        key={index}
+                        className="relative h-[140px] w-[140px] xl:h-[200px] xl:w-[200px]"
+                      >
+                        <Image src={thumb.url} fill alt={thumb.url} />
+                      </div>
+                    ))}
+                  </div>
+                  {/* text & button */}
+                  <div>
+                    <div>
+                      <h3 className="h3">{item.title}</h3>
+                      <p className="mb-10 max-w-[650px]">{item.description}</p>
+                      {/* services list */}
+                      <ul className="mb-12 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+                        {item.serviceList.map((service, index) => {
+                          return (
+                            <li key={index} className="flex items-center gap-4">
+                              <div className="h-[6px] w-[6px] bg-amber-200"></div>
+                              <div className="text-primary font-medium capitalize">
+                                {service}
+                              </div>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      {/* btn */}
+                      <Button text="Read More" />
+                    </div>
+                  </div>
+                  {/* text & button end*/}
+                </div>
+              </TabsContent>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </section>
   );
 };
 
